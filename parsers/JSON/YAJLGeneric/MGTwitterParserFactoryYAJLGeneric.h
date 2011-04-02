a0dc5d2e27985b31a1931b14a58dfae745d86fd5
Sam Deane <sam@elegantchaos.com>
2011-04-02 15:30:44 +0100

    updated copyrights

--- parsers/JSON/YAJLGeneric/MGTwitterParserFactoryYAJLGeneric.h
+++ parsers/JSON/YAJLGeneric/MGTwitterParserFactoryYAJLGeneric.h
@@ -2,7 +2,7 @@
 //  MGTwitterParserFactoryYAJLGeneric.h
 //
 //  Created by Sam Deane on 21/09/2010.
-//  Copyright 2010 Elegant Chaos. All rights reserved.
+//  Copyright 2011 Sam Deane, Elegant Chaos. All rights reserved.
 //
 
 #import "MGTwitterParserFactoryJSON.h"
